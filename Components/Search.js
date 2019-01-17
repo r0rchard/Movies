6e41357868604958678df84ed014ea181287ebb8
nicolas delcombel <ndelcombel@ensc.fr>
2019-01-17 11:32:27 +0100

    oui

---
+++ Components/Search.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, FlatList,Text } from 'react-native'
+import films from '../Helpers/filmsData'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button title='Rechercher' onPress={() => {}}/>
+        <FlatList
+          data={films}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
---
+++ Helpers/filmsData.js
@@ -0,0 +1,34 @@
+export default data =  [
+    {
+    id:4000124,
+
+      vote_average:5.3,
+
+      title:"Fast & Furious 8",
+
+      poster_path:"The fate of the furious",
+
+      original_title:"Fast & Furious 8",
+
+      overview:"Une femme manipule Dom et le fait replonger dans les activités criminelles. Son équipe se met alors à sa poursuite afin de le sauver, et empêcher un chaos mondial.",
+
+      release_date:"2017-04-12"
+
+   },
+   {
+    id:424242,
+
+      vote_average:6.0,
+
+      title:"Bumblebee",
+
+      poster_path:"",
+
+      original_title:"",
+
+      overview:"1987. Alors qu’il est en fuite, Bumblebee trouve refuge dans la décharge d’une petite ville balnéaire de Californie. Charlie (Hailee Steinfeld), à l’aube de ses 18 ans, cherche sa voie. Elle fait la connaissance de Bumblebee ; blessé par ses multiples combats, il est presque détruit.",
+
+      release_date:"2018-12-26"
+
+   },
+]
